e3d34ac44b5a18bd39799b2a377f84413595d545
Serene0324 <stan0256@student.monash.edu>
2025-10-07 22:35:53 +0800

    Rename the files

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,7 +3,7 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 
 }).catch(console.error);
 
-var vg_2 = "education_groupedbar.vg.json";
-vegaEmbed("#grouped_bar_chart", vg_2).then(function(result) {
+var vg_2 = "education_stackedbar.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
     
 }).catch(console.error);
