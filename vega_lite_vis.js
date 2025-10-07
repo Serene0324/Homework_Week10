d8b32ef175ad802d73e280f09eaa0d8ea0ea8cf0
Serene0324 <stan0256@student.monash.edu>
2025-10-07 22:34:46 +0800

    Finish the stacked bar chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,9 @@
 var vg_1 = "population_choropleth.vg.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+var vg_2 = "education_groupedbar.vg.json";
+vegaEmbed("#grouped_bar_chart", vg_2).then(function(result) {
+    
 }).catch(console.error);
